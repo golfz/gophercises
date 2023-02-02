90cdeb1dd7efc2c0b435f29bbdd3bb2bc9bf133a
surattikorn <surattikorn@gmail.com>
2023-02-02 23:06:09 +0700

    test: add Test_getQuestion

--- 01_quiz_game/main.go
+++ 01_quiz_game/main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+const defaultFileName = "problems.csv"
+
+type question struct {
+	a   int
+	b   int
+	ans int
+}
+
+func main() {
+	var fileName string
+	for i, v := range os.Args {
+		if v == "-f" {
+			fileName = os.Args[i+1]
+			break
+		}
+	}
+
+	fileName = strings.TrimSpace(fileName)
+	if fileName == "" {
+		fmt.Printf("No specify a file name, use default file: %s\n", defaultFileName)
+		fileName = defaultFileName
+	}
+
+	dat, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(dat))
+}
+
+func getQuestions(problemData string) ([]question, error) {
+	//problems := strings.Split(problemData, "\n")
+	//for i, v := range problems {
+	//
+	//}
+	return []question{}, nil
+}
+
+func getQuestion(problem string) (question, error) {
+	//ans := strings.Split(problem, ",")
+	return question{}, nil
 }
---
+++ 01_quiz_game/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_getQuestion(t *testing.T) {
+	assert.Error(t, errors.New("not implemented"))
+}
